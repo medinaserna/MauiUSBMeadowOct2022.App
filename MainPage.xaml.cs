3cb30b9ec31ff84ff43b176b7f42510f707ebcba
sebastian medina <sebastian.medina@gmail.com>
2022-10-27 14:50:37 -0700

    avances al 27oct2022

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,23 +2,24 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+	
 
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+	private void btnOpenClose_Clicked(object sender, EventArgs e)
+		{
+	
+}
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+private void btnClear_Clicked(object sender, EventArgs e)
+		{
+	
+}
+
+	
 }
 
