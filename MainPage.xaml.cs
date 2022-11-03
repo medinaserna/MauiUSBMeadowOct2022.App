32d956a9d183734537722ef046956a7381cafd20
sebastian medina <sebastian.medina@gmail.com>
2022-11-03 10:21:36 -0700

    avance 3nov 1020am

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,12 +1,23 @@
-﻿namespace MauiUSBMeadowOct2022;
+﻿using System.IO.Ports;
+
+namespace MauiUSBMeadowOct2022;
 
 public partial class MainPage : ContentPage
 {
-	
+
+	private bool bPortOpen = false;
+	private string newPacket = "";
+
 
 	public MainPage()
 	{
 		InitializeComponent();
+		string[] ports = SerialPort.GetPortNames();
+
+		foreach (string port in ports) { 
+		
+			portPicker.Items.Add(port);
+		}
 	}
 
 
@@ -19,6 +30,12 @@ private void btnClear_Clicked(object sender, EventArgs e)
 		{
 	
 }
+
+	private void btnSend_Clicked(object sender, EventArgs e) { 
+	
+	
+	}
+
 
 	
 }
