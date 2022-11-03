f2ae5cd43f993978c7b5366a1c181d455fbd692e
sebastian medina <sebastian.medina@gmail.com>
2022-11-03 10:59:26 -0700

    avance 3nov a 11am

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -7,23 +7,64 @@ public partial class MainPage : ContentPage
 
 	private bool bPortOpen = false;
 	private string newPacket = "";
+	SerialPort serialPort = new SerialPort();
 
 
 	public MainPage()
 	{
 		InitializeComponent();
 		string[] ports = SerialPort.GetPortNames();
+		portPicker.ItemsSource = ports;
+		portPicker.SelectedIndex = ports.Length;
+		Loaded += MainPage_Loaded;
 
-		foreach (string port in ports) { 
-		
-			portPicker.Items.Add(port);
-		}
+        //foreach (string port in ports) { 
+
+        //	portPicker.Items.Add(port);
+        //}
+
+       //   Console.WriteLine*/("The following serial ports were found:");
+
+        //// Display each port name to the console.
+        //foreach (string port in ports)
+        //{
+        //    Console.WriteLine(port);
+        //}
+
+        //Console.ReadLine();
+
+
+    }
+
+	private void MainPage_Loaded(object sender, EventArgs e)
+	{
+		serialPort.BaudRate = 115200;
+		serialPort.ReceivedBytesThreshold = 1;
+		serialPort.DataReceived += SerialPort_DataReceived;
 	}
 
+	private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+	{
+		newPacket = serialPort.ReadLine();
+	}
 
 	private void btnOpenClose_Clicked(object sender, EventArgs e)
 		{
-	
+		if (!bPortOpen)
+		{
+
+			serialPort.PortName = portPicker.SelectedItem.ToString();
+			serialPort.Open();
+			btnOpenClose.Text = "Close";
+			bPortOpen = true;
+		}
+		else {
+			serialPort.Close();
+			btnOpenClose.Text = "Open";
+			bPortOpen=false; 
+		
+		}
+
 }
 
 private void btnClear_Clicked(object sender, EventArgs e)
