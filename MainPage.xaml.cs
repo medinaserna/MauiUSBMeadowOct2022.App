a29731033bffd1fed570e8d919a11b1acfde20f3
sebastian medina <sebastian.medina@gmail.com>
2022-11-03 14:22:24 -0700

    actualizacion 3nov 2:22pm

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -46,6 +46,52 @@ public partial class MainPage : ContentPage
 	private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
 	{
 		newPacket = serialPort.ReadLine();
+		//labelRxdata.Text = newPacket;
+		MainThread.BeginInvokeOnMainThread(MyMainThreadCode);
+	}
+
+	private void MyMainThreadCode()
+	{
+		if (checkBoxHistory.IsChecked == true)
+		{
+
+			labelRxdata.Text = newPacket + labelRxdata.Text;
+
+		}
+		else {
+
+			labelRxdata.Text = newPacket;
+		}
+
+
+		int calChkSum = 0;
+
+		if (newPacket.Length > 37) {
+
+			if (newPacket.Substring(0, 3) == "###") {
+				string parsedData = $"{newPacket.Length, -13}"+
+                                    $"{newPacket.Substring(0,3),-12}" +
+                                    $"{newPacket.Substring(3, 3),-12}" +
+                                    $"{newPacket.Substring(6, 4),-12}" +
+                                    $"{newPacket.Substring(10, 4),-12}" +
+                                    $"{newPacket.Substring(14, 4),-12}" +
+                                    $"{newPacket.Substring(18, 4),-12}" +
+                                    $"{newPacket.Substring(22, 4),-12}" +
+                                    $"{newPacket.Substring(26, 4),-12}" +
+                                    $"{newPacket.Substring(30, 4),-12}" +
+                                    $"{newPacket.Substring(34, 3),-12}"  + "\r\n";
+
+
+				if (checkBoxParsedHistory.IsChecked == true)
+				{
+					labelParsedData.Text = parsedData + labelParsedData.Text;
+				}
+				else {
+					labelParsedData.Text = parsedData;
+				}
+
+            }
+        }
 	}
 
 	private void btnOpenClose_Clicked(object sender, EventArgs e)
