196b8b429b764d7de46d914031cebf3addacd240
sebastian medina <sebastian.medina@gmail.com>
2022-11-05 21:56:43 -0700

    avances nov5 10pm

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO.Ports;
+using System.Text;
 
 namespace MauiUSBMeadowOct2022;
 
@@ -7,6 +8,14 @@ public partial class MainPage : ContentPage
 
 	private bool bPortOpen = false;
 	private string newPacket = "";
+	private int oldPacketNumber = -1;
+	private int newPacketNumber = 0;
+	private int lostPacketCount = 0;
+	private int packetRollover = 0;
+	private int chkSumError = 0;
+
+	StringBuilder stringBuilderSend = new StringBuilder("###1111196");
+
 	SerialPort serialPort = new SerialPort();
 
 
@@ -18,29 +27,19 @@ public partial class MainPage : ContentPage
 		portPicker.SelectedIndex = ports.Length;
 		Loaded += MainPage_Loaded;
 
-        //foreach (string port in ports) { 
-
-        //	portPicker.Items.Add(port);
-        //}
-
-       //   Console.WriteLine*/("The following serial ports were found:");
-
-        //// Display each port name to the console.
-        //foreach (string port in ports)
-        //{
-        //    Console.WriteLine(port);
-        //}
-
-        //Console.ReadLine();
-
-
-    }
+	}
 
 	private void MainPage_Loaded(object sender, EventArgs e)
 	{
 		serialPort.BaudRate = 115200;
 		serialPort.ReceivedBytesThreshold = 1;
 		serialPort.DataReceived += SerialPort_DataReceived;
+		setUpSerialPort();
+	}
+
+	private void setUpSerialPort()
+	{
+		
 	}
 
 	private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
@@ -58,7 +57,8 @@ public partial class MainPage : ContentPage
 			labelRxdata.Text = newPacket + labelRxdata.Text;
 
 		}
-		else {
+		else
+		{
 
 			labelRxdata.Text = newPacket;
 		}
@@ -66,36 +66,39 @@ public partial class MainPage : ContentPage
 
 		int calChkSum = 0;
 
-		if (newPacket.Length > 37) {
+		if (newPacket.Length > 37)
+		{
 
-			if (newPacket.Substring(0, 3) == "###") {
-				string parsedData = $"{newPacket.Length, -14}"+
-                                    $"{newPacket.Substring(0,3),-14}" +
-                                    $"{newPacket.Substring(3, 3),-14}" +
-                                    $"{newPacket.Substring(6, 4),-14}" +
-                                    $"{newPacket.Substring(10, 4),-14}" +
-                                    $"{newPacket.Substring(14, 4),-14}" +
-                                    $"{newPacket.Substring(18, 4),-14}" +
-                                    $"{newPacket.Substring(22, 4),-14}" +
-                                    $"{newPacket.Substring(26, 4),-14}" +
-                                    $"{newPacket.Substring(30, 4),-14}" +
-                                    $"{newPacket.Substring(34, 3),-14}"  + "\r\n";
+			if (newPacket.Substring(0, 3) == "###")
+			{
+				string parsedData = $"{newPacket.Length,-14}" +
+									$"{newPacket.Substring(0, 3),-14}" +
+									$"{newPacket.Substring(3, 3),-14}" +
+									$"{newPacket.Substring(6, 4),-14}" +
+									$"{newPacket.Substring(10, 4),-14}" +
+									$"{newPacket.Substring(14, 4),-14}" +
+									$"{newPacket.Substring(18, 4),-14}" +
+									$"{newPacket.Substring(22, 4),-14}" +
+									$"{newPacket.Substring(26, 4),-14}" +
+									$"{newPacket.Substring(30, 4),-14}" +
+									$"{newPacket.Substring(34, 3),-14}" + "\r\n";
 
 
 				if (checkBoxParsedHistory.IsChecked == true)
 				{
 					labelParsedData.Text = parsedData + labelParsedData.Text;
 				}
-				else {
+				else
+				{
 					labelParsedData.Text = parsedData;
 				}
 
-            }
-        }
+			}
+		}
 	}
 
 	private void btnOpenClose_Clicked(object sender, EventArgs e)
-		{
+	{
 		if (!bPortOpen)
 		{
 
@@ -104,26 +107,35 @@ public partial class MainPage : ContentPage
 			btnOpenClose.Text = "Close";
 			bPortOpen = true;
 		}
-		else {
+		else
+		{
 			serialPort.Close();
 			btnOpenClose.Text = "Open";
-			bPortOpen=false; 
-		
+			bPortOpen = false;
+
 		}
 
-}
-
-private void btnClear_Clicked(object sender, EventArgs e)
-		{
-	
-}
-
-	private void btnSend_Clicked(object sender, EventArgs e) { 
-	
-	
 	}
 
+	private void btnClear_Clicked(object sender, EventArgs e)
+	{
 
-	
+	}
+
+	private void btnSend_Clicked(object sender, EventArgs e)
+	{
+
+		try
+		{
+			string messageOut = entrySend.Text;
+			messageOut += "\r\n";
+			byte[] messageBytes = Encoding.UTF8.GetBytes(messageOut);
+			serialPort.Write(messageBytes, 0, messageBytes.Length);
+		}
+		catch (Exception ex)
+		{
+			DisplayAlert("Alert", ex.Message, "Ok");
+		}
+	}
 }
 
